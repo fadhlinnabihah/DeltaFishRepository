121515af23840311a8e3de011ea148abbcc1a690
ain9024 <95596237+ain9024@users.noreply.github.com>
2022-01-09 19:03:53 +0800

    update dekat bid part
    more kepada gui

--- DeltaFishProgress1/sellerhome.php
+++ DeltaFishProgress1/sellerhome.php
@@ -37,6 +37,69 @@
                 </div>
               </div>
 
+               <div class="row">
+
+      <div class="container">
+        <div class="page-header">
+          <h2>Bidding Product List</h2>
+        </div>
+
+            
+          <?php
+          try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt = $conn->prepare("SELECT * FROM tbl_productbid_delta");
+            $stmt->execute();
+            $result = $stmt->fetchAll();
+          }
+          catch(PDOException $e){
+            echo "Error: " . $e->getMessage();
+          } ?>
+            
+          <?php
+          foreach($result as $readrow) {
+              ?>
+            <div class="col-md-3 d-flex align-items-stretch">
+            <div class="card" style=" box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); max-width: 300px;  padding: 16px;  text-align: center; font-family: arial; margin-bottom: 10px;">
+
+              <?php if(file_exists('pictures_sell/'. $readrow['PICTURE']) && isset($readrow['PICTURE'])){
+                $img = 'pictures_sell/'.$readrow['PICTURE'];
+                echo '<td><img data-toggle="modal" data-target="#'.$readrow['ID'].'" width=200px height=200px; src="'.$img.'"></td>';
+              }
+              else{
+                $img = 'nophoto.jpg';
+                echo '<td><img width=200px%; data-toggle="modal" data-target="#'.$readrow['ID'].'" src="nophoto.jpg"'.'></td>';
+              } ?>
+              
+              <p><?php echo $readrow['NAME']; ?></p>
+              <p><?php echo $readrow['DUEDATE']; ?></p>
+              <p><h2><?php echo 'RM'.$readrow['HIGHESTBID']; ?><h2></p>
+              
+<!-- 
+              <div id="<?php echo $readrow['ID']?>" class="modal fade"  role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-body">
+                  <img src="<?php echo $img ?>" class="img-responsive">
+                </div>
+              </div> -->
+
+
+              <td>
+                <a href="product_detail_bid.php?pid=<?php echo $readrow['ID']; ?>" class="btn btn-warning btn-xs" role="button">Details</a>
+                <a href="sellerhome.php?delete=<?php echo $readrow['ID']; ?>" onclick="return confirm('Are you sure to stop the bidding?');" class="btn btn-danger btn-xs" role="button">Stop</a>
+                
+              </td>
+            </div> 
+          </div>
+          
+            <?php
+          }
+          $conn = null;
+          ?>
+            </div>
+      </div>
+        </div>
+
             
 
 
@@ -46,20 +109,6 @@
         <div class="page-header">
           <h2>Products List</h2>
         </div>
-        <!-- <table id="productlist" style="width: 100%; height: 100%;" class="table-dark table-striped table-bordered hover" id="table">
-          <thead>
-          <tr>
-            <th>Product ID</th>
-            <th>Name</th>
-            <th>Description</th> 
-            <th>Price</th>
-            <th>Stock</th>
-            <th>Picture<br><small>(Clickable)</small></th>
-            <th></th>
-          </tr>
-          </thead>
-          <tbody> -->
-
 
             
           <?php
@@ -117,6 +166,9 @@
       </div>
         </div>
      
+
+
+
     
 
  
